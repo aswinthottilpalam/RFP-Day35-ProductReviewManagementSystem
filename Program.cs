8a92e2479d28522caa2278a9c837e8e1f5584fc6
aswinthottilpalam <aswinp264@gmail.com>
2022-04-09 22:28:48 +0530

    Product review management system

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management System");
+        }
+    }
+}
